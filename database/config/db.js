3bc8f7766a01f4b2fb2c84c0f3ef1da6be8f947e
abardakci <alperbardakci1441@gmail.com>
2024-11-17 09:58:25 +0300

    layers progressed 17 11 2024

--- database/config/db.js
+++ database/config/db.js
@@ -2,12 +2,10 @@ const { Sequelize } = require('sequelize');
 
 // Sequelize bağlantısını kuruyoruz
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-  host: process.env.DB_ENDPOINT, // AWS RDS veya yerel veritabanı
-  dialect: 'mysql', // Kullanılan veritabanı türü (mysql, postgres, sqlite, vb.)
-  logging: false,   // Konsola SQL sorguları yazdırmasını engellemek için
+    host: process.env.DB_ENDPOINT, // AWS RDS veya yerel veritabanı
+    dialect: 'mysql', // Kullanılan veritabanı türü (mysql, postgres, sqlite, vb.)
+    logging: false,   // Konsola SQL sorguları yazdırmasını engellemek için
 });
-
-// Bağlantıyı test etme fonksiyonu
 async function testConnection() {
   try {
     await sequelize.authenticate();
@@ -18,4 +16,3 @@ async function testConnection() {
 }
 
 module.exports = { sequelize, testConnection };
-
--- server.js
+++ server.js
@@ -2,9 +2,12 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
-const { testConnection: testDatabaseConnection } = require('./database/config/db')
+const { testConnection: testDatabaseConnection } = require('./database/config/db');
 const startNgrok = require('./utils/updateBackendUrl');
-const books = require('./dummy_data/books.json'); // Eğer 'data' klasörü altındaysa
+//const books = require('./dummy_data/books.json'); // Eğer 'data' klasörü altındaysa
+
+const bookService = require('./backend/BookService');
+const booksRoutes = require('./backend/BookController')
 
 const authRoutes = require('./routes/api/auth');
 const testRoutes = require('./routes/api/example_protected_route');
@@ -23,25 +26,11 @@ app.get('/', (req, res) => {
     res.send('Hello from' + PORT + 'port!');
 });
 
+app.use('books', booksRoutes);
+
 app.get('/api/start', (req, res) => {
     res.json({ message: "Kitaplar ekranına yönlendiriliyorsunuz" });
 });
-
-// Tüm kitapları döndüren endpoint
-app.get('/api/books', (req, res) => {
-    res.json(books);
-  });
-
-// Belirli bir ID'ye göre kitap döndüren endpoint
-app.get('/api/books/:id', (req, res) => {
-    const bookId = req.params.id;
-    const book = books.find((b) => b.id === bookId);
-    if (book) {
-      res.json(book);
-    } else {
-      res.status(404).json({ message: 'Book not found' });
-    }
-  });
 
 // Sunucuyu başlatma ve Ngrok bağlantısını başlatma
 app.listen(PORT, () => {
