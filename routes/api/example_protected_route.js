8a7bb4ca5c6c8b01e8ba8fd21462226baedff115
abardakci <alperbardakci1441@gmail.com>
2024-11-20 14:25:40 +0300

    before merge

--- routes/api/example_protected_route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const User = require('../../backend/Models/User');
-
-const authMiddleware = require('../../middlewares/authMiddleware');
-
-router.get('/', authMiddleware);
-
-module.exports = router;
