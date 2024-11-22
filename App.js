bc921fd6032e045083b8bdd18f4302e47048c3ab
MalumMRZ <yusufmrz111@gmail.com>
2024-11-22 18:56:19 +0300

    Solved system requirement conflict.

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Button, View} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './screens/HomeScreen';
@@ -9,16 +10,29 @@ import DummyScreen from './screens/DummyScreen'
 
 const Stack = createStackNavigator();
 
+
+
+
+
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'KitapPaylaş' }} />
-        <Stack.Screen name="Login" component={LoginScreen} options={{ title: 'Giriş Yap' }} />
-        <Stack.Screen name="Books" component={BooksScreen} options={{ title: 'Kitap Listesi' }} />
-        <Stack.Screen name="BookDetails" component={BookDetailsScreen} options={{ title: 'Kitap Detayları' }} />
-        <Stack.Screen name="Dummy" component={DummyScreen} options={{ title: 'Protected Route Test Ekranı' }} />
-      </Stack.Navigator>
-    </NavigationContainer>
+
+
+
+
+  return (<>
+        <View style={{display:"flex",justifyContent:"center", alignContent:"center"}}>
+        <Button title="Press">
+        </Button>
+        </View>
+      </>
+    // <NavigationContainer>
+    //   <Stack.Navigator initialRouteName="Home">
+    //     <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'KitapPaylaş' }} />
+    //     {/*<Stack.Screen name="Login" component={LoginScreen} options={{ title: 'Giriş Yap' }} />*/}
+    //     {/*<Stack.Screen name="Books" component={BooksScreen} options={{ title: 'Kitap Listesi' }} />*/}
+    //     {/*<Stack.Screen name="BookDetails" component={BookDetailsScreen} options={{ title: 'Kitap Detayları' }} />*/}
+    //     {/*<Stack.Screen name="Dummy" component={DummyScreen} options={{ title: 'Protected Route Test Ekranı' }} />*/}
+    //   </Stack.Navigator>
+    // </NavigationContainer>
   );
 }
